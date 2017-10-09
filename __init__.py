ead18ba61684bb7290b68d2b4e3e4e7a69b7064e
ubermensh <lemantar.flcl@gmail.com>
2017-10-09 19:25:15 +0300

    writing results to file (raw result + formatted)

--- __init__.py
+++ __init__.py
@@ -8,32 +8,48 @@ import json, ast, pprint
 # print(bw)
 
 # res = json.dumps({u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
-dct  = ({'site': 'porn.com', u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
+'''
+dct  = ({'site': ['porn.com'], u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
 data = ast.literal_eval(json.dumps(dct))
+print data.keys()
+site = 'aaaa'
+data['site'] = [site]
 table = PrettyTable()
-
 table.field_names = data.keys()
 row = []
 for k in data.keys():
-    row.append([data[k]])
+    row.append(" ,".join(data[k]))
 table.add_row(row)
 print table
-# table.add_row(['tesst'])
-    # table.add_row(val)
-# print table
-    
-# print(data.keys())
-# print(data['web-servers'])
+'''
 
+rawResultFile = open("rawResults.dat", "w")
+pornsites = open("short.dat", "r").read().splitlines()
+table = PrettyTable()
+FIELD_NAMES = ['site', 'web-servers', 'javascript-frameworks', 'tag-managers', 'programming-languages', 'databases', 'web-frameworks', 'cms', 'blogs', 'operating-systems', 'miscellaneous', 'video-players' ]
+table.field_names = FIELD_NAMES
+for site in pornsites:
+    print site
+    data = ast.literal_eval(json.dumps(builtwith('http://' + site)))
+    data['site'] = [site]
+    rawResultFile.write(str(data) + '/n')
 
+    row = []
+    for k in FIELD_NAMES:
+        try:
+            fieldInfo = data[k]
+        except KeyError:
+            fieldInfo = ' '
+            pass
+        row.append(" ,".join(fieldInfo))
 
-# resultFile = open("results.dat", "w")
-# pornsites = open("short.dat", "r").read().splitlines()
-# for site in pornsites:
-    # res = builtwith('http://' + site)
-    # resultFile.write(site +': ' +  res + '\n')
-    # print res
-    # exit()
+    table.add_row(row)
+
+# resultFile.write(table)
+table_txt = table.get_string()
+with open('results.dat','w') as file:
+    file.write(table_txt)
+
 
 # webpage = urlopen('https://www.cam4.com/').read()
 # soup = BeautifulSoup(webpage, 'lxml')
