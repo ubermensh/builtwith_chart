cab282aa3111bcc82dae95c38dd3dbb995477bc2
ubermensh <lemantar.flcl@gmail.com>
2017-10-09 17:36:12 +0300

    encode to utf

--- __init__.py
+++ __init__.py
@@ -1,16 +1,23 @@
 from urllib import urlopen
 from bs4 import BeautifulSoup
 from builtwith import builtwith
-bw = builtwith('http://cam4.com')
-print(bw)
+import json, ast
 
-'''
-file = open("short.dat", "r")
-pornsites = file.read().splitlines()
-for site in pornsites:
-    res = builtwith('http://' + site)
-    print res
-'''
+# bw = builtwith('http://cam4.com')
+# print(bw)
+
+# res = json.dumps({u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
+dct  = ({u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
+data = ast.literal_eval(json.dumps(dct))
+print(data.keys())
+print(data['web-servers'])
+# resultFile = open("results.dat", "w")
+# pornsites = open("short.dat", "r").read().splitlines()
+# for site in pornsites:
+    # res = builtwith('http://' + site)
+    # resultFile.write(site +': ' +  res + '\n')
+    # print res
+    # exit()
 
 # webpage = urlopen('https://www.cam4.com/').read()
 # soup = BeautifulSoup(webpage, 'lxml')
---
+++ resultExample.py
@@ -0,0 +1,5 @@
+{u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']}
+
+u'analytics': [u'Yandex.Metrika']
+
+u'font-scripts': [u'Google Font API'], u'miscellaneous': [u'Gravatar'], u'web-servers': [u'LiteSpeed'], u'javascript-frameworks': [u'jQuery'], u'programming-languages': [u'PHP'], u'web-frameworks': [u'Twitter Bootstrap'], u'cms': [u'Joomla'], u'video-players': [u'YouTube']
