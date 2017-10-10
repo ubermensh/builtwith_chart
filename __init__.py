ec4e8fb0d02af0ecc7f6af5bf86a379fcaeae7b9
ubermensh <lemantar.flcl@gmail.com>
2017-10-10 12:33:27 +0300

    refactor

--- __init__.py
+++ __init__.py
@@ -1,10 +1,21 @@
 from builtwith import builtwith
 from prettytable import PrettyTable
 import json, ast, pprint
-DOMAINS_TO_PROCESS = "input_sites/steve_list.dat"
+DOMAINS_TO_PROCESS = "input_sites/short_list.dat"
 FIELD_NAMES = ['site','analytics', 'web-servers', 'javascript-frameworks', 'tag-managers', 'programming-languages', 'databases', 'web-frameworks', 'operating-systems']
 pornsites = open(DOMAINS_TO_PROCESS, "r").read().splitlines()
 LIST_LENGTH = str(len(pornsites))
+def makeRow(data):
+    row = []
+    for k in FIELD_NAMES:
+        try:
+            fieldInfo = data[k]
+        except KeyError:
+            fieldInfo = ' '
+            pass
+        row.append(", ".join(fieldInfo))
+    return row
+
 rawResultFile = open('raw_'+ DOMAINS_TO_PROCESS[12:],'w+')
 table = PrettyTable()
 table.field_names = FIELD_NAMES
@@ -15,17 +26,10 @@ for site in pornsites:
     data = ast.literal_eval(json.dumps(builtwith('http://' + site)))
     data['site'] = [site]
     rawResultFile.write(str(data) + '\n')
-    row = []
-    for k in FIELD_NAMES:
-        try:
-            fieldInfo = data[k]
-        except KeyError:
-            fieldInfo = ' '
-            pass
-        row.append(", ".join(fieldInfo))
-
-    print str(data)
+    row = makeRow(data)
     table.add_row(row)
+    print str(data)
+
 
 table_txt = table.get_string()
 with open('result_'+ DOMAINS_TO_PROCESS[12:],'w+') as file:
