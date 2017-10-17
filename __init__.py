eb2e79cd72b4b5617282ac9e47ac4c175c26ff7a
ubermensh <lemantar.flcl@gmail.com>
2017-10-17 14:17:42 +0300

    clean up

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from builtwith import builtwith
 from prettytable import PrettyTable
 import json, ast, pprint
-DOMAINS_TO_PROCESS = "input_sites/reddit_list.dat"
+DOMAINS_TO_PROCESS = "input_sites/new.dat"
 FIELD_NAMES = ['site','analytics', 'web-servers', 'javascript-frameworks', 'tag-managers', 'programming-languages', 'databases', 'web-frameworks', 'operating-systems']
 pornsites = open(DOMAINS_TO_PROCESS, "r").read().splitlines()
 LIST_LENGTH = str(len(pornsites))
@@ -19,11 +19,10 @@ def makeRow(data):
 rawResultFile = open('raw_'+ DOMAINS_TO_PROCESS[12:],'w+')
 table = PrettyTable()
 table.field_names = FIELD_NAMES
-n = 1
-a = 5 
+n=1
 for site in pornsites:
     print str(n) + " of " + LIST_LENGTH + ": " + site
-    n += 1
+    n+=1
     data = ast.literal_eval(json.dumps(builtwith('http://' + site)))
     data['site'] = [site]
     rawResultFile.write(str(data) + '\n')
@@ -36,22 +35,3 @@ table_txt = table.get_string()
 with open('result_'+ DOMAINS_TO_PROCESS[12:],'w+') as file:
     file.write(table_txt)
 
-
-# bw = builtwith('http://cam4.com')
-# print(bw)
-
-# res = json.dumps({u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
-'''
-dct  = ({'site': ['porn.com'], u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Prototype', u'RequireJS', u'jQuery'], u'tag-managers': [u'Google Tag Manager'], u'programming-languages': [u'Java'], u'databases': [u'Firebase'], u'web-frameworks': [u'Twitter Bootstrap']})
-data = ast.literal_eval(json.dumps(dct))
-print data.keys()
-site = 'aaaa'
-data['site'] = [site]
-table = PrettyTable()
-table.field_names = data.keys()
-row = []
-for k in data.keys():
-    row.append(" ,".join(data[k]))
-table.add_row(row)
-print table
-'''
